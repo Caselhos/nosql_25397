d200a6afcdc6f726aea3d678659e064a1a8d9d16
caselhos <caselhosgoncalo@gmail.com>
2025-05-15 15:26:15 +0100

    dsa

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 app.use("/api", movieRoutes);
 
