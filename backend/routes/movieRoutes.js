4806fbf04b165841ff9df924c294d28b25424eea
caselhos <caselhosgoncalo@gmail.com>
2025-05-15 16:20:34 +0100

    das

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const movieController = require("../controllers/movieController");
 
 // Route: GET /api/movies?page=1&limit=10
+router.get("/movies/search", movieController.searchMovies);
+router.get("/movies/stats", movieController.getMovieStats);
 router.get("/movies", movieController.getMovies);
 router.get("/movies/:id", movieController.getMovieById); 
 router.post("/movies", movieController.createMovie);
 router.put("/movies/:id", movieController.updateMovie);
 router.delete("/movies/:id", movieController.deleteMovie);
-router.get("/movies/search", movieController.searchMovies);
-router.get("/movies/stats", movieController.getMovieStats);
 
 module.exports = router;
